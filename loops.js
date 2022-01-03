5d795d84e36065265f8519fd4bf89e7511cd91d6
Kevin Dansie <56943194+Dansiedesign@users.noreply.github.com>
2022-01-03 16:21:00 -0700

    Update loops.js

--- loops.js
+++ loops.js
@@ -7,7 +7,7 @@
 */
 
 
- let i =1;
+ let i =0;
 
  while (i<100) {
    i++
@@ -25,42 +25,3 @@
     console.log(i);
   }
  }
-
-// let i = 1;
-//   do {
-//     i++
-//     console.log(i);
-
-//   while (i<= 100, i % 5 === 0 && i%3 === 0) {
-//     i++      
-//     console.log('Fizzbuzz');
-//   }
-//   while ( i<= 100, i % 3 === 0) {
-//     i++
-//     console.log('fizz');
-//   }
-//   while (i<= 100, i % 5 === 0) {
-//     i++
-//     console.log('buzz');
-//   }
-// }
-//   while (i<= 100);
-//   console.log(i);
-
-
-  // for(let i = 1; i <= 100; i++){
-
-  //   if(i % 3 === 0 && i % 5 === 0){
-  //     console.log("FizzBuzz");
-  //    }
-  //   else if (i % 3 === 0){
-  //     console.log("Fizz");
-  //   }
-  //   else if (i % 5 === 0){
-  //     console.log("Buzz");
-  //   }
-  //   else{
-  //     console.log(i);
-  //   }
-  //  }
-  
